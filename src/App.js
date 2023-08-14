5ba43112f2ed448ae8a6c1448bbaad0d310e2890
stastnan <nik.stastna@gmail.com>
2023-08-14 16:13:36 +0200

    Added global styles, typography

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
+import { GlobalStyles } from "./styles/Global";
+
+import {
+  ButtonText,
+  MainTitle,
+  SectionSubtitle,
+  SectionTitle,
+  SubText,
+  Text,
+} from "./components/ui/Typography";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <MainTitle>Hello</MainTitle>
+      <SectionTitle>Hello</SectionTitle>
+      <SectionSubtitle>Hello</SectionSubtitle>
+      <Text>Hello</Text>
+      <SubText>Hello</SubText>
+      <ButtonText>Hello</ButtonText>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/ui/Typography.js
@@ -0,0 +1,43 @@
+import { styled } from "styled-components";
+
+export const MainTitle = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: 60px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: 90px;
+`;
+
+export const SectionTitle = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: 35px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: 52px;
+`;
+
+export const SectionSubtitle = styled.h3`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: 25px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: normal;
+`;
+
+export const Text = styled.span`
+  font-family: ${({ theme }) => theme.fonts.inter}, sans-serif;
+  font-size: 22px;
+  font-weight: ${({ theme }) => theme.fontWeights.semibold};
+  line-height: normal;
+`;
+
+export const SubText = styled.span`
+  font-family: ${({ theme }) => theme.fonts.inter}, sans-serif;
+  font-size: 20px;
+  font-weight: ${({ theme }) => theme.fontWeights.normal};
+  line-height: normal;
+`;
+
+export const ButtonText = styled.span`
+  font-family: ${({ theme }) => theme.fonts.inter}, sans-serif;
+  font-size: 23px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: normal;
+`;
